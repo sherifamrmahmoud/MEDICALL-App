33b5d86c94b8fa588af9c047370ebafbfefe76b7
Sherif Amer <sherifamer072@gmail.com>
2026-05-11 04:21:20 +0300

    final project update

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,17 @@ import 'package:nti5/auth/cubit/auth_cubit.dart';
 import 'package:nti5/auth/login/login_screen.dart';
 import 'package:nti5/auth/signup/signup_screen.dart';
 import 'package:nti5/core/data_source/firebase_data_source.dart';
-import 'package:nti5/features/Home/home_screen.dart';
+import 'package:nti5/features/Home/cubit/home_cubit.dart';
+
+import 'package:nti5/features/Home/screens/home_screen.dart';
 import 'package:nti5/features/OnboardingScreen/OnboardingScreen.dart';
 
 import 'package:nti5/features/Splash%20Screen/splash_screen.dart';
+import 'package:nti5/features/My%20Appointments/appointments_screen.dart';
+import 'package:nti5/features/doctor_diagnoses/diagnosis_summary_screen.dart';
+import 'package:nti5/features/find_doctor/cubit/find_doctor_cubit.dart';
+import 'package:nti5/features/find_doctor/screens/find_doctor_screen.dart';
+import 'package:nti5/features/profile/ui/profile_screen.dart';
 import 'package:nti5/firebase_options.dart';
 
 void main() async {
@@ -37,7 +44,24 @@ class MyApp extends StatelessWidget {
           create: (context) => AuthCubit(FirebaseDataSource()),
           child: LoginScreen(),
         ),
-        '/home': (context) => HomeScreen(),
+
+        '/home': (context) => BlocProvider(
+          create: (_) => HomeCubit()..loadHome(),
+
+          child: HomeScreen(),
+        ),
+        '/find': (context) => BlocProvider(
+          create: (_) => FindDoctorCubit()..loadDoctors(),
+
+          child: FindDoctorScreen(),
+        ),
+
+        '/appointment': (context) => AppointmentsScreen(),
+        '/diagnosis': (context) => DiagnosisSummaryScreen(),
+
+
+        '/ProfileScreen': (context) => ProfileScreen(),
+
       },
       builder: (context, child) {
         return MediaQuery(
